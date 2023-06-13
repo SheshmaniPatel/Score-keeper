b07e9184959631aa52f9930d77a638884afa7c90
Sheshmani Patel <sumitkumar47581@gmail.com>
2023-06-13 10:04:46 +0530

    updating score and wicket with condition

--- index.js
+++ index.js
@@ -1,32 +1,49 @@
-var score=0;
-var wicket=0;
+var score = 0;
+var wicket = 0;
+var ballwiseResult=[];
 
- 
-function clickscore(num){
-    score +=num;
-    scoreRoot.render(<App/>);
-}
- 
-function clickwicket(){
-    wicket +=1;
-    scoreRoot.render(<App/>);
+function clickscore(num) {
+  if (wicket === 10) {
+    return;
+  }
+  ballwiseResult.push(num);
+  score += num;
+  scoreRoot.render(<App />);
+  console.log(ballwiseResult)
 }
 
-const App =()=>(
-    <>
+function clickwicket() {
+  if (wicket === 10) {
+    return;
+  }
+  ballwiseResult.push("W");
+  wicket += 1;
+  scoreRoot.render(<App />);
+  console.log(ballwiseResult)
+}
+
+const AllButtons = () => (
+  <div>
+    <button onClick={() => clickscore(0)}>0</button>
+    <button onClick={() => clickscore(1)}>1</button>
+    <button onClick={() => clickscore(2)}>2</button>
+    <button onClick={() => clickscore(3)}>3</button>
+    <button onClick={() => clickscore(4)}>4</button>
+    <button onClick={() => clickscore(5)}>5</button>
+    <button onClick={() => clickscore(6)}>6</button>
+    <button onClick={clickwicket}>Wicket</button>
+  </div>
+);
+
+const App = () => (
+  <>
     <h1>Score Keeper</h1>
-    <h2>Score : {score}/{wicket}</h2>
-    <div  >
-        <button onClick={ ()=>clickscore(1)} >1</button>
-        <button onClick={()=>clickscore(2)}>2</button>
-        <button  onClick={()=>clickscore(3)}>3</button>
-        <button onClick={()=>clickscore(4)}>4</button>
-        <button  onClick={()=>clickscore(5)}>5</button>
-        <button onClick={()=>clickscore(6)}>6</button>
-        <button onClick={clickwicket}>Wicket</button>
-    </div>
-    </>
-)
+    <h2>
+      Score : {score}/{wicket}
+    </h2>
+    <AllButtons />
+  </>
+);
 
-const scoreRoot=ReactDOM.createRoot(document.getElementById("root"));
-scoreRoot.render(<App/>);
+const scoreRoot = ReactDOM.createRoot(document.getElementById("root"));
+scoreRoot.render(<App />);
